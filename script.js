72fadb0f8478390efe9906793114fe9de5bd925a
OmerAkgull <143122832+OmerAkgull@users.noreply.github.com>
2023-12-27 18:55:53 +0300

    Create arithmetic functions

---
+++ script.js
@@ -0,0 +1,20 @@
+//arithmetic functions
+function sum(a, b) {
+  return a + b;
+}
+console.log(sum(1, 3));
+
+function subtract(a, b) {
+  return a - b;
+}
+console.log(subtract(5, 4));
+
+function multiply(a, b) {
+  return a * b;
+}
+console.log(multiply(5, 10));
+
+function divide(a, b) {
+  return a / b;
+}
+console.log(divide(60, 15));
